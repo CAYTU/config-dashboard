3e8b65fa2b67f3d682ded997fb037ade8d2e2f59
sudoping01 <sudoping01@gmail.com>
2025-09-25 23:13:49 +0000

    main entrypoint

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ESPHomeConfigGenerator from './dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <ESPHomeConfigGenerator />
+    </div>
+  );
+}
+
+export default App;
